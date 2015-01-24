e456e3753aa6e4db4530a9c32f878c1795024c48
Jonathan Scott <jonathan@jscott.me>
2015-01-24 22:37:13 +0000

    Add basic animation

---
+++ Assets/Scripts/FollowCharacter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Make the camera follow the character around the scene
+ */
+public class FollowCharacter : MonoBehaviour {
+
+	public Transform target;
+
+	void LateUpdate () {
+		transform.position = new Vector3(target.position.x, transform.position.y, transform.position.z);
+	}
+}
---
+++ Assets/Scripts/SimpleMovementController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleMovementController : MonoBehaviour {
+
+	static float speed = 1;
+
+	bool dead = false;
+
+	void Update () {
+		SkeletonAnimation s = this.GetComponent<SkeletonAnimation>();
+		if (!dead) {
+			if (Input.GetKey ("left")) {
+				transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, transform.position.y, transform.position.z);
+				s.state.SetAnimation(0, "walk", true);
+			} else if (Input.GetKey ("right")) {
+				transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
+				s.state.SetAnimation(0, "walk", true);
+			} else if (Input.GetKey ("up")) {
+				transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + speed * Time.deltaTime);
+				s.state.SetAnimation(0, "walk", true);
+			} else if (Input.GetKey ("down")) {
+				transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - speed * Time.deltaTime);
+				s.state.SetAnimation(0, "walk", true);
+			} else if (Input.GetKey ("f")) {
+				s.state.SetAnimation(0, "death", false);
+				dead = true;
+			} else {
+				s.state.ClearTracks();
+			}
+		}
+
+	}
+}
