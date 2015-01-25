ebdeb25f21b581fb4cc9fffb69290d1468872b08
Jonathan Scott <jonathan@jscott.me>
2015-01-25 04:39:33 +0000

    Milk that falls

---
+++ Assets/Scripts/Shelves.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shelves : MonoBehaviour {
+
+	void OnCollisionEnter(Collision c) {
+		if (c.gameObject.tag == "WillNotPush") {
+			return;
+		}
+		foreach (Transform child in transform){
+			foreach (Transform child2 in child){
+				Debug.Log (child2.gameObject.tag);
+				if(child2.gameObject.tag == "Fallable"){
+					child2.rigidbody.isKinematic = false;
+				}
+			}
+		}
+	}
+}
